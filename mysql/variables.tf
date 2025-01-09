db83839f7c7aebdf2a656b2b380bae6d56349fee
tusharaws072 <tusharaws072@gmail.com>
2025-01-09 11:59:20 +0530

    Update variables.tf

--- mysql/variables.tf
+++ mysql/variables.tf
@@ -13,24 +13,4 @@ variable "sns_email" {
 }
  
 
- variable "aws_region" {
-  description = "The AWS region where resources will be created"
-  type        = string
-  default     = "ap-southeast-1" 
-}
-
-
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  sensitive   = true
-  default     = ""
-
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret access key"
-  sensitive   = true
-  default     = ""
-
-}
+ 
